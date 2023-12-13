1cbd1723629d9a9b58d1714af526418074b8b63a
Talant Kosmosbekov <t1kosmos1@gmail.com>
2023-12-13 18:37:58 +0600

    upload all files

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import requests
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY") #write in the terminal : export OWM_API_KEY=the key (this is to secure keys)
+account_sid = "YOUR ACCOUNT SID"
+auth_token = os.environ.get("AUTH_TOKEN")
+
+parameters = {
+    "lat": 42.88,
+    "lon": 74.58,
+    "appid": "cae3a0fa073cba3209553513fd705eec",
+     "cnt": 4
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+#my version
+# conditions = []
+# for i in range(0, 4):
+#     weather_condition = weather_data["list"][i]["weather"][0]["id"]
+#     conditions.append(weather_condition)
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
+
+
